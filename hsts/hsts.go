be157d703603c1598e8eb15e29c83c5ba888f29d
Steve Phillips <steve@tryingtobeawesome.com>
2017-03-20 17:21:56 -0700

    Initial commit. Added .gitignore, gosecure.go, and {canary,csp,hsts}/*.go

---
+++ hsts/hsts.go
@@ -0,0 +1,29 @@
+package hsts
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const oneYearInSeconds = 31536000 // 365 * 24 * 60 * 60
+
+func Handler(h http.Handler) http.Handler {
+	preload := false
+	return CustomHandler(h, preload, oneYearInSeconds)
+}
+
+func PreloadHandler(h http.Handler) http.Handler {
+	preload := true
+	return CustomHandler(h, preload, oneYearInSeconds)
+}
+
+func CustomHandler(h http.Handler, preload bool, maxAgeInSecs int) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		header := fmt.Sprintf("max-age=%d; includeSubDomains", maxAgeInSecs)
+		if preload {
+			header = "; preload"
+		}
+		w.Header().Add("Strict-Transport-Security", header)
+		h.ServeHTTP(w, req)
+	})
+}
