74e4249ef1c384fc6939bb1b23d85e95fdc38029
Steve Phillips <steve@tryingtobeawesome.com>
2017-03-20 19:18:54 -0700

    autocert now caches the generated SSL cert(s) in a local dir

--- gosecure.go
+++ gosecure.go
@@ -79,6 +79,7 @@ func getTLSConfig(domain string) *tls.Config {
 	m := autocert.Manager{
 		Prompt:     autocert.AcceptTOS,
 		HostPolicy: autocert.HostWhitelist(domain),
+		Cache:      autocert.DirCache("./" + domain),
 	}
 
 	return &tls.Config{
