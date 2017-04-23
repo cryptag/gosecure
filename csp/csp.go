ef05d0815bd451a9b9bec270a9629bd4230f955c
Steve Phillips <steve@tryingtobeawesome.com>
2017-04-23 00:07:15 -0700

    csp: Fonts and media can now be loaded from -domain without violating CSP headers

--- csp/csp.go
+++ csp/csp.go
@@ -15,7 +15,7 @@ func GetHandler(domain string) func(h http.Handler) http.Handler {
 func GetCustomHandler(domain, apiDomain string) func(h http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			w.Header().Set("Content-Security-Policy", fmt.Sprintf("default-src 'none'; script-src https://%s; style-src https://%[1]s; img-src https://%[1]s; connect-src https://%s; child-src 'self'", domain, apiDomain))
+			w.Header().Set("Content-Security-Policy", fmt.Sprintf("default-src 'none'; script-src https://%s; style-src https://%[1]s; img-src https://%[1]s; font-src https://%[1]s; media-src https://%[1]s; connect-src https://%s; child-src 'self'", domain, apiDomain))
 			h.ServeHTTP(w, req)
 		})
 	}
