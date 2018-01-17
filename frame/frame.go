77621e25dc2903fb33696b6e81aef7a270166d49
Steve Phillips <steve@tryingtobeawesome.com>
2018-01-16 22:23:41 -0800

    Added frame.GetHandler(domain, port string)

--- frame/frame.go
+++ frame/frame.go
@@ -4,10 +4,25 @@
 package frame
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/cryptag/gosecure/set"
 )
+
+func GetHandler(domain, port string) func(h http.Handler) http.Handler {
+	var maybeColonPort string
+	if port != "" {
+		maybeColonPort = ":" + port
+	}
+
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			w.Header().Set("X-Frame-Options", fmt.Sprintf("ALLOW-FROM https://%s%s", domain, maybeColonPort))
+			h.ServeHTTP(w, req)
+		})
+	}
+}
 
 // SameOriginHandler is middleware that sets the header `X-Frame-Options: SAMEORIGIN`.
 func SameOriginHandler(h http.Handler) http.Handler {
